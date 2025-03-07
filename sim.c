b0713cea70d632edeb6cb220bc6e3cd41e7f87f7
Cyprian Siwik <cyprian_siwik@student.uml.edu>
2025-03-07 15:34:56 -0500

    Update sim.c

--- sim.c
+++ sim.c
@@ -25,3 +25,12 @@ void process_instruction()
   decode();
   execute();
 }
+
+
+10000917    # auipc x18, 0x10000
+00090913    # addi x18, x18, 0
+015ea023    # sw x21, 0(x29)
+002e1e93    # slli x29, x28, 2
+015a0b33    # add x22, x20, x21
+013e2f33    # slt x30, x28, x19
+fe0f10e3    # bne x30, x0, -32
